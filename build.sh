af52fae5b6062f73b977f550150315bf78fda12c
Solomon Wood <solomon.wood@hotmail.com>
2024-11-17 08:08:05 +0000

    Updates to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cp -r pkg pkg.tmp
 chmod -R 755 pkg.tmp/
-chown root -R pkg.tmp
+sudo -S chown -R root pkg.tmp
 
 dpkg -b pkg.tmp
 mv pkg.tmp.deb me.ylur.itunesstorexv.deb
-rm -rf pkg.tmp
+sudo rm -rf pkg.tmp
